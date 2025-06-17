541a87c8696be788ab5901b535b3d58ec9e8c064
zalmanleib <154308683+zalmanleib@users.noreply.github.com>
2025-06-17 04:19:46 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = "zmanim-app-cache-v1";
+const filesToCache = ["/", "/index.html", "/manifest.json", "/your-icon.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
